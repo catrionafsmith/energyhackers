dc413e18350e074b45a0ae7452d42cce098db4ba
Katy Smith <catriona.f.smith@gmail.com>
2023-07-01 22:38:49 +0100

    Updated form page

--- public/script.js
+++ public/script.js
@@ -7,14 +7,14 @@ const outputContainer = document.getElementById("output-container")
 if (emailButton) {
     emailButton.addEventListener('click', async () => {
         loadingButton.style.display = 'block'
-        loadingButton.scrollIntoView({ behavior: 'smooth' })
-        // const getSelectedMember = document.querySelector('input[name="communityMembers"]:checked')
-        // console.log(getSelectedMember)
-        // console.log("Button clicked")
+        // loadingButton.scrollIntoView({ behavior: 'smooth' })
+        const getSelectedTravel = document.querySelector('input[name="travel"]:checked')
+        const getSelectedDrink = document.querySelector('input[name="drink-input"]:checked')// console.log(getSelectedMember)
+        const getSelectedMeal = document.querySelector('input[name="food-option"]:checked')// console.log("Button clicked")
         // const text = document.getElementById("textareaspace").value
         // console.log(text)
-        const prompt = `you are a helpful and positive bot with a friendly tone who gives information about a user's carbon footprint at an event in central london. I say that I walked to this event, and brought a reusable coffee cup with me and am a vegetarian. You reply to give me a carbon score (out of 10) and some advice about how I can further reduce my environmental impact while at the event (e.g. by turning off devices when not in use and by using half flushes):`
-        // console.log(prompt)
+        const prompt = `you are a helpful and positive bot with a friendly tone who gives information about a user's carbon footprint at an event in central london. I say that I will go to this event by ${getSelectedTravel.id}, I drink from a ${getSelectedDrink.id} cup and I mainly eat a ${getSelectedMeal.id} diet. You reply to give me a carbon emissions score (in kg of CO2 emission) and some advice about how I can further reduce my environmental impact while at the event (e.g. by turning off devices when not in use and by using half flushes):`
+        console.log(prompt)
     
         const keyresp = await fetch('/.netlify/functions/get-token')
         .then(response => response.json()
