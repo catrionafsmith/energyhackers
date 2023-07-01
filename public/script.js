c2f2de1009ebf46c6e76201cae3f1a447fa71652
Katy Smith <catriona.f.smith@gmail.com>
2023-07-01 17:38:45 +0100

    Update js file

--- public/script.js
+++ public/script.js
@@ -8,13 +8,13 @@ if (emailButton) {
     emailButton.addEventListener('click', async () => {
         loadingButton.style.display = 'block'
         loadingButton.scrollIntoView({ behavior: 'smooth' })
-        const getSelectedMember = document.querySelector('input[name="communityMembers"]:checked')
+        // const getSelectedMember = document.querySelector('input[name="communityMembers"]:checked')
         // console.log(getSelectedMember)
-        // console.log("Button clicked")
-        const text = document.getElementById("textareaspace").value
+        console.log("Button clicked")
+        // const text = document.getElementById("textareaspace").value
         // console.log(text)
-        const prompt = `you are a helpful and positive bot with a friendly tone who gives information about a user's carbon footprint at an event in central london. I say that I walked to this event, and brought a reusable coffee cup with me and am a vegetarian. You reply to give me a carbon score (out of 10) and some advice about how I can reduce my environmental impact further:`
-        // console.log(prompt)
+        const prompt = `you are a helpful and positive bot with a friendly tone who gives information about a user's carbon footprint at an event in central london. I say that I walked to this event, and brought a reusable coffee cup with me and am a vegetarian. You reply to give me a carbon score (out of 10) and some advice about how I can further reduce my environmental impact while at the event (e.g. by turning off devices when not in use and by using half flushes):`
+        console.log(prompt)
     
         const keyresp = await fetch('/.netlify/functions/get-token')
         .then(response => response.json()
@@ -31,7 +31,7 @@ if (emailButton) {
                     },
                         }
             ).then((response) => {
-                console.log(text)
+                // console.log(text)
                 if (response.ok) {
                     response.json().then((json) => {
                         output.textContent = json.choices[0].text.trim();
